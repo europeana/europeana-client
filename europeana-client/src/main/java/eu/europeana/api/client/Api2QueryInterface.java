a9e8d9d0cd95e63763061cbf3452742755ee7c46
bozicb <bojan.bozic@ait.ac.at>
2014-06-06 15:11:07 +0200

    javadoc extended

--- europeana-client/src/main/java/eu/europeana/api/client/Api2QueryInterface.java
+++ europeana-client/src/main/java/eu/europeana/api/client/Api2QueryInterface.java
@@ -3,22 +3,29 @@ package eu.europeana.api.client;
 import java.util.List;
 
 /**
- * The query interface should be used instead of concrete implementation in order to allow use case specific enhancements
- * @author Sergiu Gordea 
+ * Interface definition for standardized Search API calls.
+ * 
+ * @author Sergiu Gordea
+ * @version 1.0
+ * 
+ * The query interface should be used instead of concrete implementation in order 
+ * to allow use case specific enhancements
  *
  */
 public interface Api2QueryInterface extends EuropeanaQueryInterface {
 
 	/**
 	 * Registers a query refinement
-	 * Similar to the "add keyword" functionality in the portal  
-	 * @param qf
+	 * Similar to the "add keyword" functionality in the portal
+	 *   
+	 * @param qf: string to define the query refinement
 	 */
 	public void addQueryRefinement(String qf);
 
 	/**
 	 * retrieves the list of registered query refinements
-	 * @return
+	 * 
+	 * @return list of registered query refinements
 	 */
 	public List<String> getQueryRefinements();
 
