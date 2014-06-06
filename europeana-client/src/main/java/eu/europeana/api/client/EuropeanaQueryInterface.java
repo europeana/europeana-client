137a8952f526946123a9bf87dfb479508e9ba9c0
bozicb <bojan.bozic@ait.ac.at>
2014-06-06 15:34:11 +0200

    javadoc extended

--- europeana-client/src/main/java/eu/europeana/api/client/EuropeanaQueryInterface.java
+++ europeana-client/src/main/java/eu/europeana/api/client/EuropeanaQueryInterface.java
@@ -5,8 +5,15 @@ import java.io.UnsupportedEncodingException;
 import eu.europeana.api.client.connection.EuropeanaConnection;
 
 /**
+ * Standard interface for Europeana queries.
  *
  * @author Cosmin Coman
+ * @version 1.0
+ * 
+ * This inferface provides the basic functionality for
+ * general queries to the Europeana database. It defines
+ * methods for querying URLs and retrieving search terms
+ * of a query.
  */
 public interface EuropeanaQueryInterface {
 
@@ -20,8 +27,8 @@ public interface EuropeanaQueryInterface {
     /**
      * Returns the full Europeana query url
      * 
-     * @param connection
-     * @return
+     * @param connection: defines the used Europeana connection object.
+     * @return string representing the query URL of a search.
      * @throws UnsupportedEncodingException 
      */
     public String getQueryUrl(EuropeanaConnection connection) throws UnsupportedEncodingException;
@@ -29,9 +36,9 @@ public interface EuropeanaQueryInterface {
     /**
      * Returns the full Europeana query url
      * 
-     * @param connection
-     * @param offset
-     * @return
+     * @param connection: defines the used Europeana connection object.
+     * @param offset: The item in the search results to start with; first item is 1 [default = 1].
+     * @return string representing the query URL of a search.
      * @throws UnsupportedEncodingException 
      */
     public String getQueryUrl(EuropeanaConnection connection, long offset) throws UnsupportedEncodingException;
@@ -39,26 +46,61 @@ public interface EuropeanaQueryInterface {
     /**
      * Returns the full Europeana query url
      * 
-     * @param connection
-     * @param limit The number of records to return; the maximum value is 100 [default = 12].
-     * @param offset The item in the search results to start with; first item is 1 [default = 1].
-     * @return
+     * @param connection: defines the used Europeana connection object.
+     * @param limit: The number of records to return; the maximum value is 100 [default = 12].
+     * @param offset: The item in the search results to start with; first item is 1 [default = 1].
+     * @return string representing the query URL of a search.
      * @throws UnsupportedEncodingException 
      */
     public String getQueryUrl(EuropeanaConnection connection, long limit, long offset) throws UnsupportedEncodingException;
     
+    /**
+     * Retrieves the used query type.
+     * 
+     * @return used query type.
+     */
     public String getType();
 
+    /**
+     * Sets the used query type.
+     * 
+     * @param type: used query type.
+     */
     public void setType(String type);
     
+    /**
+     * Sets the terms for a query.
+     * 
+     * @param what: terms for the query.
+     */
     public void setWhatTerms(String what);
 
+    /**
+     * Sets general query terms.
+     * 
+     * @param generalTerms: string containing general terms.
+     */
 	public void setGeneralTerms(String generalTerms);
 
+	/**
+	 * Sets the creator of the query.
+	 * 
+	 * @param creator: string containing query creator.
+	 */
 	public void setCreator(String creator);
 
+	/**
+	 * Sets the provider of a query.
+	 * 
+	 * @param provider: string containing the provider.
+	 */
 	public void setProvider(String provider);
 	
+	/**
+	 * Sets the data provider of the query.
+	 * 
+	 * @param dataProvider: string containing the data provider.
+	 */
 	public void setDataProvider(String dataProvider);
 	
 }
