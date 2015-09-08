126ba1e794ae9263950a1699d51b313bb887c4e2
Sergiu Gordea <sergiu.gordea@ait.ac.at>
2015-09-08 16:31:13 +0200

    #26 refactoring package structure

---
+++ europeana-client/src/main/java/eu/europeana/api/client/model/ProviderAccessResponse.java
@@ -0,0 +1,8 @@
+package eu.europeana.api.client.model;
+
+import eu.europeana.api.client.model.provider.Provider;
+import eu.europeana.api.client.response.abstracts.AbstractObjectAccessResponse;
+
+public class ProviderAccessResponse extends AbstractObjectAccessResponse<Provider>{
+
+}
---
+++ europeana-client/src/main/java/eu/europeana/api/client/model/ProvidersResponse.java
@@ -0,0 +1,9 @@
+package eu.europeana.api.client.model;
+
+import eu.europeana.api.client.model.provider.Provider;
+import eu.europeana.api.client.response.abstracts.AbstractListResponse;
+
+public class ProvidersResponse extends AbstractListResponse<Provider>{
+
+
+}
